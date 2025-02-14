20507a48cc3354f81d7cdac7a14613d1b8f15bc2
DESKTOP-NQO1TN8\Oscar <oscar_gs19@hotmail.com>
2025-02-14 21:14:22 +0100

    Capitulo ngModel de Angular

--- proyecto001/src/app/app.component.ts
+++ proyecto001/src/app/app.component.ts
@@ -8,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto001';
+  nombre = 'pedro';
+  edad = 12;
+  fumador = false;
+  sueldos = [1200,1500,1700];
+  provincias=[
+    {id:1, nombre:"Huelva"},
+    {id:2, nombre:"Sevilla"},
+    {id:3, nombre:"Almeria"},
+    {id:4, nombre:"Cordoba"},
+    {id:5, nombre:"Cadiz"}
+  ];
+
+  contador = 0;
+
+  esFumador (){
+    return this.fumador?"Es fumador":"No es fumador";
+  }
+
+  diHola(){
+    alert('Hola');
+  }
+
+  incrementar(){
+    this.contador++;
+  }
+
+  decrementar(){
+    this.contador--;
+  }
 }
+
