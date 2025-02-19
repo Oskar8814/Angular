57c1fd24b0412c3dd8d5087adb34fc86c45af5ce
DESKTOP-NQO1TN8\Oscar <oscar_gs19@hotmail.com>
2025-02-19 22:22:08 +0100

    Configuracion de BS5

--- proyecto038/src/app/app.component.ts
+++ proyecto038/src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+declare var $:any; // Le garantizamos a TypeScript que $ existe
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'proyecto038';
+
+  
+ngOnInit(): void {
+    $('[data-bs-toggle="popover"]').popover();
+}
+
+
+aceptar(){
+  alert("Ha pulsado Aceptar!");
+}
+
+cancelar(){
+  alert("Ha pulsado Cancelar!");
+}
 }
